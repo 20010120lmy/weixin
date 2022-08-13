543d2d9659e184799e4d9e78fdb8ce405d0f0924
20010120lmy <111163337+20010120lmy@users.noreply.github.com>
2022-08-13 10:02:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wx2f355389c7214874"
 # 公众号appSecret
-app_secret = ""
+app_secret = "0876ae9789d1ee82d610bd20168ddb05"
 # 模板消息id
-template_id = ""
+template_id = "G9pz9vd8W2bc3V6-gTMOyPuAL7TmFC1ieQToDtdCkZ4"
 # 接收公众号消息的微信号
-user = ""
+user = "owYfp576tVequXTpNI3GfZSfni-8"
 
 # 信息配置
 # 所在省份
-province = "广东"
+province = "山东"
 # 所在城市
-city = "广州"
-# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1997-1-1"
+city = "济南"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2021-9-20"
 
